8adf98fb63e26c9bb4781b3fd92329d28c315724
Britt Shook <brittanysshook@gmail.com>
2023-12-01 18:39:10 -0500

    remove comments with assignment instructions

--- index.js
+++ index.js
@@ -76,40 +76,7 @@ const LearnerSubmissions = [
     }
 ];
 
-// Your goal is to analyze and transform this data such that the output of your program is an
-// array of objects, each containing the following information in the following format:
-// {
-//     // the ID of the learner for which this data has been collected
-//     "id": number,
-//     // the learner’s total, weighted average, in which assignments
-//     // with more points_possible should be counted for more
-//     // e.g. a learner with 50/100 on one assignment and 190/200 on another
-//     // would have a weighted average score of 240/300 = 80%.
-//     "avg": number,
-//     // each assignment should have a key with its ID,
-//     // and the value associated with it should be the percentage that
-//     // the learner scored on the assignment (submission.score / points_possible)
-//     <assignment_id>: number,
-//     // if an assignment is not yet due, it should not be included in either
-//     // the average or the keyed dictionary of scores
-// }
-
-// Similar data validation should occur elsewhere within the program.
-// You should also account for potential errors in the data that your program 
-// receives. What if points_possible is 0? You cannot divide by zero. What if a 
-// value that you are expecting to be a number is instead a string? 
-// Use try/catch and other logic to handle these types of errors gracefully.
-// If an assignment is not yet due, do not include it in the results or the 
-// average. Additionally, if the learner’s submission is late (submitted_at is
-// past due_at), deduct 10 percent of the total points possible from their score
-// for that assignment. Create a function named getLearnerData() that accepts
-// these values as parameters, in the order listed: (CourseInfo, AssignmentGroup, 
-// [LearnerSubmission]), and returns the formatted result, which should be an 
-// array of objects as described above.
-// Use at least two if/else statements to control program flow. Optionally, use at least one switch statement.
-// Utilize at least two different types of loops.
-
-
+// Helper Functions
 function setDefault(obj, key, value) {
     if (!obj[key]) {
         obj[key] = value;
@@ -124,6 +91,7 @@ function getAssignmentInfo(assignmentId, assignmentGroup) {
     }
 }
 
+// Main Function
 function getLearnerData(courseInfo, assignmentGroup, learnerSubmissions) {
     try {
         if (courseInfo.id == assignmentGroup.course_id) {
